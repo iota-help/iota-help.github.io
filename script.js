3f4f7becd6af493e8914439d6452ee6efb0ef774
Sebastian Kremer <1644266+OMSKremer@users.noreply.github.com>
2023-09-19 17:05:08 +0200

    - general design changes - added "Unlock Schedule" table

--- script.js
+++ script.js
@@ -39,41 +39,91 @@ function sanitizeInput(input) {
                 .replace(/'/g, '&#039;');
 }
 
+function generateUnlockSchedule(totalTokens) {
+    const initialUnlock = totalTokens * 0.1;
+    const biWeeklyUnlock = (totalTokens - initialUnlock) / (24 * 2); // 24 months, bi-weekly
+
+    let schedule = [];
+    let currentDate = new Date(2023, 9, 4); // October 4th, 2023
+    let remainingTokens = totalTokens;
+
+    // Initial unlock
+    schedule.push({
+        date: currentDate.getDate().toString().padStart(2, '0') + '.' + (currentDate.getMonth() + 1).toString().padStart(2, '0') + '.' + currentDate.getFullYear(),
+        tokens: initialUnlock,
+        remaining: remainingTokens - initialUnlock
+    });
+
+    remainingTokens -= initialUnlock;
+
+    for (let i = 0; i < 24 * 2; i++) {
+        currentDate.setDate(currentDate.getDate() + 14);
+        schedule.push({
+            date: currentDate.getDate().toString().padStart(2, '0') + '.' + (currentDate.getMonth() + 1).toString().padStart(2, '0') + '.' + currentDate.getFullYear(),
+            tokens: biWeeklyUnlock,
+            remaining: remainingTokens - biWeeklyUnlock
+        });
+        remainingTokens -= biWeeklyUnlock;
+    }
+
+    return schedule;
+}
+
+
 function checkAddresses() {
     const rawInput = document.getElementById("addressInput").value;
-    const sanitizedInput = sanitizeInput(rawInput);
-    const userInput = sanitizedInput.trim().split('\n');
-    let resultsArray = [];
-    let totalValue = 0;
+    
+    if (rawInput.trim() !== "") {
+        document.getElementById("totalSumHeader").style.display = "block";
+        document.getElementById("totalTable").style.display = "table";
+        
+        document.getElementById("unlockScheduleHeader").style.display = "block";
+        document.getElementById("unlockScheduleTable").style.display = "table";
+        
+        document.getElementById("resultsHeader").style.display = "block";
+        document.getElementById("resultsTable").style.display = "table";
 
-    for (let line of userInput) {
-        const [address, _] = line.split(',');
-        let value = addresses[address] || 0;
-        resultsArray.push({ address, value });
-        totalValue += value;
+        const sanitizedInput = sanitizeInput(rawInput);
+        const userInput = sanitizedInput.trim().split('\n');
+        let resultsArray = [];
+        let totalValue = 0;
+
+        for (let line of userInput) {
+            const [address, _] = line.split(',');
+            let value = addresses[address] || 0;
+            resultsArray.push({ address, value });
+            totalValue += value;
+        }
+
+        resultsArray.sort((a, b) => b.value - a.value);
+
+        let output = "";
+        for (let result of resultsArray) {
+            output += `<tr><td>${result.address}</td><td>${formatNumber(result.value).originalFormat}</td></tr>`;
+        }
+
+        const formattedTotal = formatNumber(totalValue);
+        document.getElementById("total").innerHTML = `
+            <tr>
+                <td>(OLD = Chrysalis in base tokens) IOTAs</td>
+                <td>${formattedTotal.originalFormat}</td>
+            </tr>
+            <tr>
+                <td>OR (OLD = Chrysalis in metric units) <b>MIOTA</b></td>
+                <td>${formattedTotal.miotaFormat}</td>
+            </tr>
+            <tr>
+                <td>OR (NEW = Stardust units) <b>IOTA.micros</b></td>
+                <td>${formattedTotal.iotaFormat}</td>
+            </tr>
+        `;
+        document.getElementById("results").innerHTML = output;
+
+        const unlockSchedule = generateUnlockSchedule(totalValue);
+        let unlockOutput = "";
+        for (let entry of unlockSchedule) {
+            unlockOutput += `<tr><td>${entry.date}</td><td>${formatNumber(entry.tokens).iotaFormat}</td><td>${formatNumber(entry.remaining).iotaFormat}</td></tr>`;
+        }
+        document.getElementById("unlockSchedule").innerHTML = unlockOutput;
     }
-
-    resultsArray.sort((a, b) => b.value - a.value);
-
-    let output = "";
-    for (let result of resultsArray) {
-        output += `<tr><td>${result.address}</td><td>${formatNumber(result.value).originalFormat}</td></tr>`;
-    }
-
-	const formattedTotal = formatNumber(totalValue);
-	document.getElementById("total").innerHTML = `
-		<tr>
-			<td>(OLD = Chrysalis in base tokens) IOTAs</td>
-			<td>${formattedTotal.originalFormat}</td>
-		</tr>
-		<tr>
-			<td>OR (OLD = Chrysalis in metric units) MIOTA</td>
-			<td>${formattedTotal.miotaFormat}</td>
-		</tr>
-		<tr>
-			<td>OR (NEW = Stardust units) IOTA.micros</td>
-			<td>${formattedTotal.iotaFormat}</td>
-		</tr>
-	`;
-	document.getElementById("results").innerHTML = output;
 }
